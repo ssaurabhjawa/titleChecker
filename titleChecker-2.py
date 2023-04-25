a3969d8ad057f2554da6d170fe11d607c7cab1ce
Saurabh Jawa <ssaurabhjawa@gmail.com>
2023-04-25 18:19:59 +0530

    titcleChecker-2

--- titleChecker-2.py
+++ titleChecker-2.py
@@ -1,8 +1,10 @@
 import os
 import csv
 import tkinter as tk
-from tkinter import filedialog, ttk, Listbox, Canvas, NW
+from tkinter import filedialog, ttk, Listbox, Canvas, NW, END
 from PIL import Image, ImageTk
+import csv
+
 
 # Initialize tkinter app
 root = tk.Tk()
@@ -11,6 +13,8 @@ root.title("Image Renaming App")
 # Define global variables
 image_folder = ""
 completed_renaming = []
+renamed_files = []
+
 
 
 
@@ -89,10 +93,130 @@ option_value_entry.pack(side="top", padx=5, pady=5, anchor="nw")
 
 
 
+# Create the renamed_listbox
+renamed_listbox = tk.Listbox(root, width=50)
+renamed_listbox.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+def rename_file():
+    # Get selected image filename
+    selected_file = image_listbox.get(image_listbox.curselection())
+
+    # Create new filename
+    new_filename = f"{vendor_var.get()}--{product_type_var.get()}--{title_var.get()}--{option_name_var.get()}-{option_value_var.get()}"
+    
+    # Rename file
+    os.rename(os.path.join(image_folder, selected_file), os.path.join(image_folder, new_filename))
+
+    # Update image_listbox
+    image_listbox.delete(image_listbox.curselection())
+
+    # Add new filename to renamed_files array
+    renamed_files.append(new_filename)
+
+    # Update renamed_listbox
+    renamed_listbox.delete(0, END)
+    for file in renamed_files:
+        renamed_listbox.insert(END, file)
+
+rename_button = tk.Button(root, text="Rename File", command=rename_file)
+rename_button.pack()
 
 
+import csv
+import tkinter as tk
+from tkinter import filedialog
+
+def create_csv():
+    # Prompt user for save location
+    file_path = filedialog.asksaveasfilename(defaultextension='.csv', filetypes=[('CSV files', '*.csv')])
+
+    # Define fields for CSV file
+    fieldnames = [
+        'Handle',
+        'Title',
+        'Body (HTML)',
+        'Vendor',
+        'Product Category',
+        'Type',
+        'Tags',
+        'Published',
+        'Option1 Name',
+        'Option1 Value',
+        'Option2 Name',
+        'Option2 Value',
+        'Option3 Name',
+        'Option3 Value',
+        'Variant SKU',
+        'Variant Grams',
+        'Variant Inventory Tracker',
+        'Variant Inventory Qty',
+        'Variant Inventory Policy',
+        'Variant Fulfillment Service',
+        'Variant Price',
+        'Variant Compare At Price',
+        'Variant Requires Shipping',
+        'Variant Taxable',
+        'Variant Barcode',
+        'Image Src',
+        'Image Position',
+        'Image Alt Text',
+        'Gift Card',
+        'SEO Title',
+        'SEO Description',
+        'Google Shopping / Google Product Category',
+        'Google Shopping / Gender',
+        'Google Shopping / Age Group',
+        'Google Shopping / MPN',
+        'Google Shopping / AdWords Grouping',
+        'Google Shopping / AdWords Labels',
+        'Google Shopping / Condition',
+        'Google Shopping / Custom Product',
+        'Google Shopping / Custom Label 0',
+        'Google Shopping / Custom Label 1',
+        'Google Shopping / Custom Label 2',
+        'Google Shopping / Custom Label 3',
+        'Google Shopping / Custom Label 4',
+        'Variant Image',
+        'Variant Weight Unit',
+        'Variant Tax Code',
+        'Cost per item',
+        'Included / United Arab Emirates',
+        'Included / International',
+        'Price / International',
+        'Compare At Price / International',
+        'Status'
+    ]
+
+    # Create CSV file and write header row
+    with open(file_path, mode='w', newline='') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+
+        # Write data from renamed files to CSV file
+        for file_name in renamed_files:
+            # Split file name into data fields using '--' delimiter
+            data = file_name.split('--')
+
+            # Write data fields to CSV file
+            writer.writerow({
+                'Handle': data[0],
+                'Title': data[2],
+                'Vendor': data[0],
+                'Product Category': '',
+                'Type': data[1],
+                'Option1 Name': data[3],
+                'Option1 Value': data[4],
+                'Image Src': '',
+                'Image Position': '',
+                'Image Alt Text': '',
+            })
+
+    # Notify user that file was created
+    tk.messagebox.showinfo('CSV Created', 'CSV file created successfully.')
 
 
+create_csv_button = tk.Button(root, text='Create CSV', command=create_csv)
+create_csv_button.pack(side=tk.LEFT, padx=10, pady=10)
 
 
 
