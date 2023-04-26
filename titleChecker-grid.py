228eaad230d5fe9415c3bcb8c61a1fc5d1fc8afc
Saurabh Jawa <ssaurabhjawa@gmail.com>
2023-04-26 17:10:22 +0530

    df

--- titleChecker-grid.py
+++ titleChecker-grid.py
@@ -5,7 +5,7 @@ from tkinter import filedialog, ttk, Listbox, Canvas, NW, END, messagebox
 from PIL import Image, ImageTk
 import csv
 import shutil
-
+from PIL import Image
 
 # Initialize tkinter app
 root = tk.Tk()
@@ -17,17 +17,16 @@ completed_renaming = []
 renamed_files = []
 
 
-# # Configure rows and columns with grid_columnconfigure and grid_rowconfigure
-# for i in range(5):
-#     root.grid_columnconfigure(i, weight=1, minsize=50)
-#     root.grid_rowconfigure(i, weight=1, minsize=50)
+# Configure rows and columns with grid_columnconfigure and grid_rowconfigure
+for i in range(10):
+    root.grid_columnconfigure(i, weight=1, minsize=50)
+    root.grid_rowconfigure(i, weight=1, minsize=50)
 
-# # Create widgets with grid
-# for i in range(5):
-#     for j in range(5):
-#         label = tk.Label(root, text=f"({i}, {j})", borderwidth=1, relief="solid")
-#         label.grid(row=i, column=j, padx=1, pady=1, sticky="nsew")
-
+# Create widgets with grid
+for i in range(10):
+    for j in range(10):
+        label = tk.Label(root, text=f"({i}, {j})", borderwidth=1, relief="solid")
+        label.grid(row=i, column=j, padx=1, pady=1, sticky="nsew")
 
 def select_folder():
     global image_folder, image_files
@@ -45,7 +44,6 @@ count_label.grid(row=0, column=0, padx=5, pady=10, sticky="sw")
 # Create button to select folder
 select_folder_button = tk.Button(root, text="Select Folder", command=select_folder)
 select_folder_button.grid(row=7, column=0, padx=5, pady=5)
-
 
 # Create listbox to display image files
 image_listbox = tk.Listbox(root,width=100)
@@ -71,20 +69,12 @@ def show_image(event):
 image_listbox.bind('<<ListboxSelect>>', show_image)
 
 # Define input variables
-vendor_var = tk.StringVar(root)
-vendor_options = ["Vendor 1", "Vendor 2", "Vendor 3", "Vendor 4"]
 product_type_var = tk.StringVar(root)
-product_type_options = ["Type 1", "Type 2", "Type 3", "Type 4"]
+product_type_options = ["Canvas", "Acrylic", "Mugs", "T-Shirts","Wall Paper", "Poster", "NoteBook", "ArtBook"]
 title_var = tk.StringVar(root)
-option_name_var = tk.StringVar(root)
-option_name_options = ["Option 1", "Option 2", "Option 3", "Option 4"]
-option_value_var = tk.StringVar(root)
+# Create variable for image position
+image_position_var = tk.IntVar(value=0)
 
-
-vendor_label = tk.Label(root, text="Vendor:")
-vendor_dropdown = tk.OptionMenu(root, vendor_var, *vendor_options)
-vendor_label.grid(row=2, column=0, padx=5, pady=5, sticky="w")
-vendor_dropdown.grid(row=3, column=0, padx=5, pady=5, sticky="w")
 
 product_type_label = tk.Label(root, text="Product Type:")
 product_type_dropdown = tk.OptionMenu(root, product_type_var, *product_type_options)
@@ -96,27 +86,41 @@ title_entry = tk.Entry(root, textvariable=title_var, width=100)
 title_label.grid(row=0, column=0, padx=5, pady=1, sticky="sw")
 title_entry.grid(row=1, column=0, columnspan=4, padx=5, pady=1, sticky="w")
 
-option_name_label = tk.Label(root, text="Option Name:")
-option_name_dropdown = tk.OptionMenu(root, option_name_var, *option_name_options)
-option_name_label.grid(row=2, column=2, padx=5, pady=1, sticky="w")
-option_name_dropdown.grid(row=3, column=2, padx=5, pady=1, sticky="w")
-
-option_value_label = tk.Label(root, text="Option Value:")
-option_value_entry = tk.Entry(root, textvariable=option_value_var)
-option_value_label.grid(row=2, column=3, padx=5, pady=1, sticky="w")
-option_value_entry.grid(row=3, column=3, padx=5, pady=1, sticky="w")
-
 # Create the renamed_listbox
 renamed_listbox = tk.Listbox(root, width=100)
 renamed_listbox.grid(row=4, column=0,columnspan=4, padx=10, pady=10)
+
+def create_output_folder():
+    global output_folder_path
+    output_folder_path = filedialog.askdirectory(title="Select output folder")
+    if output_folder_path:
+        os.makedirs(output_folder_path, exist_ok=True)
+        tk.messagebox.showinfo("Success", f"Output folder created at {output_folder_path}")
+        # Display list of image files in output_listbox
+        output_files = [f for f in os.listdir(output_folder_path) if f.endswith('.jpg') or f.endswith('.jpeg') or f.endswith('.png')]
+        output_listbox.delete(0, tk.END)
+        for file in output_files:
+            output_listbox.insert(tk.END, file)
+    else:
+        tk.messagebox.showerror("Error", "No output folder selected")
+
+# Create Output Folder button
+create_output_folder_button = tk.Button(root, text="Select Output Folder", command=create_output_folder)
+create_output_folder_button.grid(row=7, column=1, padx=10, pady=10)
 
 
 def rename_file():
     # Get selected image filename
     selected_file = image_listbox.get(image_listbox.curselection())
 
-    # Create new filename
-    new_filename = f"{vendor_var.get()}--{product_type_var.get()}--{title_var.get()}--{option_name_var.get()}-{option_value_var.get()}{os.path.splitext(selected_file)[1]}"
+    # Get image aspect ratio
+    img_path = os.path.join(image_folder, selected_file)
+    with Image.open(img_path) as img:
+        width, height = img.size
+        aspect_ratio = round(width / height, 2)
+
+    # Create new filename with aspect ratio
+    new_filename = f"{product_type_var.get()}--{title_var.get()}--{aspect_ratio}--{image_position_var.get()}{os.path.splitext(selected_file)[1]}"
 
     try:
         # Rename file
@@ -138,137 +142,106 @@ def rename_file():
 
     except Exception as e:
         tk.messagebox.showerror("Error", f"An error occurred while renaming the file: {e}")
-             # Print error message to console for debugging
+        # Print error message to console for debugging
         print(f"Error occurred while renaming the file: {str(e)}")
+
 
 # "Rename File" and binds it to the function 'rename_file'.
 rename_button = tk.Button(root, text="Rename File", command=rename_file)
 rename_button.grid(row=7, column=2, padx=5, pady=1)
 
+# Listbox widget to display the output image files and set its width
+output_listbox = tk.Listbox(root, width=70)
+output_listbox.grid(row=0, column=6, padx=5, pady=1)
+
+# Create a label to display the selected name
+selected_label = tk.Label(root, text="Selected Name: ")
+selected_label.grid(row=2, column=2, columnspan=5, padx=5, pady=5, sticky="w")
+
+# Function to update the selected name label
+def update_selected_name(event):
+    # Get the selected name from the listbox
+    selected_name = output_listbox.get(output_listbox.curselection())
+    # Update the selected name label
+    selected_label.config(text=f"Selected Name: {selected_name}")
+
+# Bind the ListboxSelect event to the update_selected_name function
+output_listbox.bind("<<ListboxSelect>>", update_selected_name)
+
+# Create new Entry widget for highest image position
+highest_image_position_entry = tk.Entry(root, width=50)
+highest_image_position_entry.grid(row=6, column=1, padx=5, pady=5, sticky="w")
 
 
-def create_csv():
-    # Prompt user for save location
-    file_path = filedialog.asksaveasfilename(defaultextension='.csv', filetypes=[('CSV files', '*.csv')])
+def find_name():
+    # Get the selected filename from the output listbox
+    selected_filename = output_listbox.get(output_listbox.curselection())
 
-    # Define fields for CSV file
-    fieldnames = [
-        'Handle',
-        'Title',
-        'Body (HTML)',
-        'Vendor',
-        'Product Category',
-        'Type',
-        'Tags',
-        'Published',
-        'Option1 Name',
-        'Option1 Value',
-        'Option2 Name',
-        'Option2 Value',
-        'Option3 Name',
-        'Option3 Value',
-        'Variant SKU',
-        'Variant Grams',
-        'Variant Inventory Tracker',
-        'Variant Inventory Qty',
-        'Variant Inventory Policy',
-        'Variant Fulfillment Service',
-        'Variant Price',
-        'Variant Compare At Price',
-        'Variant Requires Shipping',
-        'Variant Taxable',
-        'Variant Barcode',
-        'Image Src',
-        'Image Position',
-        'Image Alt Text',
-        'Gift Card',
-        'SEO Title',
-        'SEO Description',
-        'Google Shopping / Google Product Category',
-        'Google Shopping / Gender',
-        'Google Shopping / Age Group',
-        'Google Shopping / MPN',
-        'Google Shopping / AdWords Grouping',
-        'Google Shopping / AdWords Labels',
-        'Google Shopping / Condition',
-        'Google Shopping / Custom Product',
-        'Google Shopping / Custom Label 0',
-        'Google Shopping / Custom Label 1',
-        'Google Shopping / Custom Label 2',
-        'Google Shopping / Custom Label 3',
-        'Google Shopping / Custom Label 4',
-        'Variant Image',
-        'Variant Weight Unit',
-        'Variant Tax Code',
-        'Cost per item',
-        'Included / United Arab Emirates',
-        'Included / International',
-        'Price / International',
-        'Compare At Price / International',
-        'Status'
-    ]
+    # Split filename into its components
+    parts = selected_filename.split("--")
+    product_type = parts[0]
+    title = parts[1]
+    aspect_ratio = parts[2]
 
-    # Create CSV file and write header row
-    with open(file_path, mode='w', newline='') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-
-        # Write data from renamed files to CSV file
-        for file_name in renamed_files:
-            # Split file name into data fields using '--' delimiter
-            data = file_name.split('--')
-
-            # Write data fields to CSV file
-            writer.writerow({
-                'Handle': data[0],
-                'Title': data[2],
-                'Vendor': data[0],
-                'Product Category': '',
-                'Type': data[1],
-                'Option1 Name': data[3],
-                'Option1 Value': data[4],
-                'Image Src': '',
-                'Image Position': '',
-                'Image Alt Text': '',
-            })
-
-    # Notify user that file was created
-    tk.messagebox.showinfo('CSV Created', 'CSV file created successfully.')
-
-
-
-
-# Create CSV button
-create_csv_button = tk.Button(root, text='Create CSV', command=create_csv)
-create_csv_button.grid(row=7, column=3, padx=10, pady=10)
-
-
-
-def create_output_folder():
-    global output_folder_path
-    output_folder_path = filedialog.askdirectory(title="Select output folder")
-    if output_folder_path:
-        os.makedirs(output_folder_path, exist_ok=True)
-        tk.messagebox.showinfo("Success", f"Output folder created at {output_folder_path}")
+    # Find existing files with the same product type, title, and aspect ratio in output folder
+    output_files = [f for f in os.listdir(output_folder_path) if f.startswith(f"{product_type}--{title}--{aspect_ratio}")]
+    image_positions = [int(os.path.splitext(f.split("--")[3])[0]) for f in output_files]
+    if len(image_positions) == 0:
+        next_image_position = 1
     else:
-        tk.messagebox.showerror("Error", "No output folder selected")
+        next_image_position = max(image_positions) + 1
 
-# Create Output Folder button
-create_output_folder_button = tk.Button(root, text="Create Output Folder", command=create_output_folder)
-create_output_folder_button.grid(row=7, column=1, padx=10, pady=10)
+    # Update the title_entry with the next available image position
+    highest_image_position_entry.delete(0, END)
+    highest_image_position_entry.insert(0, f"{product_type}--{title}--{aspect_ratio}--{next_image_position}")
 
-
-
-
-
+    # Set the focus on the option_value_entry for easy editing
+    highest_image_position_entry.focus()
 
 
 
+find_name_button = tk.Button(root, text="Find Name", command=find_name)
+find_name_button.grid(row=4, column=5, padx=5, pady=5, sticky="w")
+
+
+def rename_file_with_position():
+    # Get selected image filename
+    selected_file = image_listbox.get(image_listbox.curselection())
+
+    # Get the new filename with updated image position
+    new_filename = f"{highest_image_position_entry.get()}{os.path.splitext(selected_file)[1]}"
+
+    try:
+        # Rename file
+        os.rename(os.path.join(image_folder, selected_file), os.path.join(image_folder, new_filename))
+
+        # Copy file to output folder with new filename
+        shutil.copy(os.path.join(image_folder, new_filename), os.path.join(output_folder_path, new_filename))
+
+        # Remove file from image_listbox
+        image_listbox.delete(image_listbox.curselection())
+
+        # Add new filename to renamed_files array
+        renamed_files.append(new_filename)
+
+        # Update renamed_listbox
+        renamed_listbox.delete(0, END)
+        for file in renamed_files:
+            renamed_listbox.insert(END, file)
+
+        # Update output_listbox
+        output_listbox.insert(END, new_filename)
+
+    except Exception as e:
+        tk.messagebox.showerror("Error", f"An error occurred while renaming the file: {e}")
+        # Print error message to console for debugging
+        print(f"Error occurred while renaming the file: {str(e)}")
 
 
 
-
-
+# Button to rename selected image file with highest image position from entry box
+rename_with_position_button = tk.Button(root, text="Rename File with Position", command=rename_file_with_position)
+rename_with_position_button.grid(row=10, column=1, padx=10, pady=10)
 
 
 # Run the main event loop
