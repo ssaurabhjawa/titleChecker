f7fb0dac17b65847577ab9fad9d84f89c7ecd34e
Saurabh Jawa <ssaurabhjawa@gmail.com>
2023-04-27 13:01:16 +0530

    ice cream

--- titleChecker-grid-1.py
+++ titleChecker-grid-1.py
@@ -52,7 +52,7 @@ image_listbox.grid(row=0, column=0,columnspan=4, padx=5, pady=1, sticky="w")
 
 # Create Canvas to display image
 image_canvas = tk.Canvas(root, width=400, height=400)
-image_canvas.grid(row=0, column=5, padx=5, pady=1, sticky="w")
+image_canvas.grid(row=0, column=2, padx=5, pady=1, sticky="w")
 
 # Function to display selected image
 def show_image(event):
@@ -83,56 +83,6 @@ def update_selected_image(event):
     # Set the focus back to the previously selected item in image_listbox
     image_listbox.activate(image_listbox.curselection())
 
-
-# Create new Listbox widget to hold current selection
-current_selection_listbox = tk.Listbox(root, height=1, width=100)
-current_selection_listbox.grid(row=1, column=1, padx=5, pady=5, sticky="w")
-
-def update_current_selection(event):
-    # Get selected file name
-    selected_file = image_listbox.get(image_listbox.curselection())
-
-    # Update current selection Listbox
-    current_selection_listbox.delete(0, tk.END)
-    current_selection_listbox.insert(0, selected_file)
-
-# Bind the image_listbox to update the current_selection_listbox
-image_listbox.bind('<Double-Button-1>', update_current_selection)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Define input variables
 product_type_options = ["Canvas", "Acrylic", "Mugs", "T-Shirts","Wall Paper", "Poster", "NoteBook", "ArtBook"]
 product_type_var = tk.StringVar(root, product_type_options[0])
@@ -157,9 +107,10 @@ title_entry = tk.Entry(root, textvariable=title_var, width=100)
 title_label.grid(row=2, column=0, padx=5, pady=1, sticky="e")
 title_entry.grid(row=2, column=1, columnspan=2, padx=5, pady=1, sticky="w")
 
+
 # Create the renamed_listbox
 renamed_listbox = tk.Listbox(root, width=100)
-renamed_listbox.grid(row=4, column=0,columnspan=4, padx=10, pady=10)
+renamed_listbox.grid(row=6, column=0,columnspan=4, padx=10, pady=10)
 
 def create_output_folder():
     global output_folder_path
@@ -177,12 +128,18 @@ def create_output_folder():
 
 # Create Output Folder button
 create_output_folder_button = tk.Button(root, text="Select Output Folder", command=create_output_folder)
-create_output_folder_button.grid(row=7, column=1, padx=10, pady=10)
-
+create_output_folder_button.grid(row=4, column=1, padx=10, pady=10)
 
 def rename_file():
+    # Activate current_selection_listbox
+    current_selection_listbox.focus_set()
+
     # Get selected image filename
     selected_file = current_selection_listbox.get(current_selection_listbox.curselection())
+
+    if not selected_file:
+        tk.messagebox.showerror("Error", "Please select an image to rename.")
+        return
 
     # Get image aspect ratio
     img_path = os.path.join(image_folder, selected_file)
@@ -195,14 +152,14 @@ def rename_file():
 
     try:
         # Rename file
-        os.rename(os.path.join(image_folder, selected_file), os.path.join(image_folder, new_filename))
-
-        # Copy file to output folder with new filename
-        shutil.copy(os.path.join(image_folder, new_filename), os.path.join(output_folder_path, new_filename))
+        os.rename(os.path.join(image_folder, selected_file), os.path.join(output_folder_path, new_filename))
 
         # Update current_selection_listbox
         current_selection_listbox.delete(0, tk.END)
         current_selection_listbox.insert(0, new_filename)
+
+        # Clear current_selection_listbox
+        current_selection_listbox.selection_clear(0, tk.END)
 
         # Update image_listbox
         image_listbox.delete(0, tk.END)
@@ -212,6 +169,7 @@ def rename_file():
 
         # Add new filename to renamed_files array
         renamed_files.append(new_filename)
+
 
         # Update renamed_listbox
         renamed_listbox.delete(0, END)
@@ -224,68 +182,35 @@ def rename_file():
         print(f"Error occurred while renaming the file: {str(e)}")
 
 
-# def rename_file():
-#     # Get selected image filename
-#     selected_file = image_listbox.get(image_listbox.curselection())
+# Create new Listbox widget to hold current selection
+current_selection_listbox = tk.Listbox(root, height=1, width=100)
+current_selection_listbox.grid(row=1, column=1, padx=5, pady=5, sticky="w")
 
-#     # Get image aspect ratio
-#     img_path = os.path.join(image_folder, selected_file)
-#     with Image.open(img_path) as img:
-#         width, height = img.size
-#         aspect_ratio = round(width / height, 2)
+def update_current_selection(event):
+    # Get selected file name
+    selected_file = image_listbox.get(image_listbox.curselection())
 
-#     # Create new filename with aspect ratio
-#     new_filename = f"{product_type_var.get()}--{title_var.get()}--{aspect_ratio}--{image_position_var.get()}{os.path.splitext(selected_file)[1]}"
+    # Update current selection Listbox
+    current_selection_listbox.delete(0, tk.END)
+    current_selection_listbox.insert(0, selected_file)
 
-#     try:
-#         # Rename file
-#         os.rename(os.path.join(image_folder, selected_file), os.path.join(image_folder, new_filename))
+# Bind the image_listbox to update the current_selection_listbox
+image_listbox.bind('<Double-Button-1>', update_current_selection)
 
-#         # Copy file to output folder with new filename
-#         shutil.copy(os.path.join(image_folder, new_filename), os.path.join(output_folder_path, new_filename))
-
-#         # Update image_listbox
-#         image_listbox.delete(image_listbox.curselection())
-
-#         # Add new filename to renamed_files array
-#         renamed_files.append(new_filename)
-
-#         # Update renamed_listbox
-#         renamed_listbox.delete(0, END)
-#         for file in renamed_files:
-#             renamed_listbox.insert(END, file)
-
-#     except Exception as e:
-#         tk.messagebox.showerror("Error", f"An error occurred while renaming the file: {e}")
-#         # Print error message to console for debugging
-#         print(f"Error occurred while renaming the file: {str(e)}")
-
+# Bind the current_selection_listbox to rename_file function
+current_selection_listbox.bind('<<ListboxSelect>>', rename_file)
 
 # "Rename File" and binds it to the function 'rename_file'.
 rename_button = tk.Button(root, text="Rename File", command=rename_file)
-rename_button.grid(row=3, column=2, padx=5, pady=1)
+rename_button.grid(row=2, column=2, padx=5, pady=1)
 
 # Listbox widget to display the output image files and set its width
 output_listbox = tk.Listbox(root, width=70,height=20)
-output_listbox.grid(row=0, column=6, padx=5, pady=1)
-
-# Create a label to display the selected name
-selected_label = tk.Label(root, text="Selected Name: ")
-selected_label.grid(row=2, column=5, columnspan=5, padx=5, pady=5, sticky="nw")
-
-# Function to update the selected name label
-def update_selected_name(event):
-    # Get the selected name from the listbox
-    selected_name = output_listbox.get(output_listbox.curselection())
-    # Update the selected name label
-    selected_label.config(text=f"Selected Name: {selected_name}")
-
-# Bind the ListboxSelect event to the update_selected_name function
-output_listbox.bind("<<ListboxSelect>>", update_selected_name)
+output_listbox.grid(row=0, column=3, padx=5, pady=1)
 
 # Create new Entry widget for highest image position
-highest_image_position_entry = tk.Entry(root, width=50)
-highest_image_position_entry.grid(row=6, column=1, padx=5, pady=5, sticky="w")
+highest_image_position_entry = tk.Entry(root, width=70)
+highest_image_position_entry.grid(row=2, column=3, padx=5, pady=5, sticky="w")
 
 
 def find_name():
@@ -316,36 +241,53 @@ def find_name():
 
 
 find_name_button = tk.Button(root, text="Use Name", command=find_name)
-find_name_button.grid(row=2, column=7, padx=5, pady=5, sticky="N")
+find_name_button.grid(row=3, column=3, padx=5, pady=5, sticky="w")
 
 
 def rename_file_with_position():
-    # Get selected image filename
-    selected_file = image_listbox.get(image_listbox.curselection())
+    # Get selected file from the current selection Listbox
+    selected_file = current_selection_listbox.get(current_selection_listbox.curselection())
+    img_path = os.path.normpath(os.path.join(image_folder, selected_file))
+    print(img_path)
 
-    # Get the new filename with updated image position
-    new_filename = f"{highest_image_position_entry.get()}{os.path.splitext(selected_file)[1]}"
+    if not selected_file:
+        tk.messagebox.showerror("Error", "Please select an image to rename.")
+        return
 
+    # Get the new filename from the highest_image_position_entry input box
+    new_filename = highest_image_position_entry.get()
+
+    # Rename the file with the new filename
     try:
-        # Rename file
-        os.rename(os.path.join(image_folder, selected_file), os.path.join(image_folder, new_filename))
+        os.rename(img_path, os.path.join(image_folder, new_filename))
 
         # Copy file to output folder with new filename
         shutil.copy(os.path.join(image_folder, new_filename), os.path.join(output_folder_path, new_filename))
 
-        # Remove file from image_listbox
-        image_listbox.delete(image_listbox.curselection())
+        # Update current selection Listbox
+        current_selection_listbox.delete(0, tk.END)
+        current_selection_listbox.insert(0, new_filename)
 
-        # Add new filename to renamed_files array
-        renamed_files.append(new_filename)
+        # Clear current_selection_listbox
+        current_selection_listbox.selection_clear(0, tk.END)
 
-        # Update renamed_listbox
-        renamed_listbox.delete(0, END)
-        for file in renamed_files:
-            renamed_listbox.insert(END, file)
+        # Update image_listbox
+        image_listbox.delete(0, tk.END)
+        for file in os.listdir(image_folder):
+            if file.endswith((".jpg", ".jpeg", ".png")):
+                image_listbox.insert(tk.END, file)
 
         # Update output_listbox
-        output_listbox.insert(END, new_filename)
+        output_listbox.delete(0, tk.END)
+        for file in os.listdir(output_folder_path):
+            if file.endswith((".jpg", ".jpeg", ".png")):
+                output_listbox.insert(tk.END, file)
+
+        # Update renamed_listbox
+        renamed_files.append(new_filename)
+        renamed_listbox.delete(0, tk.END)
+        for file in renamed_files:
+            renamed_listbox.insert(tk.END, file)
 
     except Exception as e:
         tk.messagebox.showerror("Error", f"An error occurred while renaming the file: {e}")
@@ -353,10 +295,24 @@ def rename_file_with_position():
         print(f"Error occurred while renaming the file: {str(e)}")
 
 
+# Define function to update the output listbox
+def update_output_listbox():
+    output_listbox.delete(0, tk.END)
+    for file in os.listdir(output_folder_path):
+        if file.endswith((".jpg", ".jpeg", ".png")):
+            output_listbox.insert(tk.END, file)
+
+# Create button to update the output listbox
+update_output_button = tk.Button(root, text="Update Output Listbox", command=update_output_listbox)
+update_output_button.grid(row=4, column=2, padx=5, pady=5)
+
 
 # Button to rename selected image file with highest image position from entry box
-rename_with_position_button = tk.Button(root, text="Rename File with Position", command=rename_file_with_position)
-rename_with_position_button.grid(row=20, column=1, padx=10, pady=10)
+rename_with_position_button = tk.Button(root, text="Rename File with Next Position", command=rename_file_with_position)
+rename_with_position_button.grid(row=3, column=3, padx=10, pady=10,sticky='e')
+
+
+
 
 
 # Run the main event loop
